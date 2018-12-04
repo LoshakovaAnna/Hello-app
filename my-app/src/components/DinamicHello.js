1657072e0accf95140dd7dbb287cace308fd98e9
LoshakovaAnna <ann.loshakova.111@gmail.com>
2018-12-04 20:13:33 +0300

    use React, render, getElementById, create classDinamicHello ext Component, onChange input :  setState.helloText

---
+++ my-app/src/components/DinamicHello.js
@@ -0,0 +1,33 @@
+import React, { Component} from 'react';
+import './style.css';
+
+
+class  DinamicHello  extends Component {
+    state = { 
+        helloText : "Hello, stranger!"
+    }
+    render() {
+        return (
+            <div className="hello-block">
+                <input type="text" id="inputName"  className="input-name" onChange={this.handleNameChange}
+                placeholder="input your name"></input>
+                <p className="hello-string">{this.state.helloText} </p>
+            </div>
+        );
+    };
+    
+    handleNameChange = () =>{
+        var  valName = document.getElementById('inputName').value;
+      
+        var helloString;
+        if (valName) {
+            helloString = "Hello, "  + valName + "!";
+        }
+        else
+            helloString = "Hello, stranger!";
+        this.setState({ helloText : helloString});
+    }
+    
+};
+
+ export default DinamicHello;
---
+++ my-app/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {render} from 'react-dom';
+import DinamicHello from './components/DinamicHello';
+
+
+
+render(<DinamicHello />, document.getElementById('root'));
